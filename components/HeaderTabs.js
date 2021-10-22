427ac8a4fa0b3eed2a5bc311428a162caa8594d4
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-22 23:51:59 +0000

    Initial commit

---
+++ components/HeaderTabs.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export default function 
+() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
