32ac0769a1c09beb94f522cd46656879c37d0496
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-23 23:36:30 +0000

    Completed restaurant item section for each restaurant

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,56 @@
-import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
-import Categories from '../components/Categories'
-import HeaderTabs from '../components/HeaderTabs'
-import SearchBar from '../components/SearchBar'
+import React, { useState, useEffect } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import Categories from "../components/Categories";
+import HeaderTabs from "../components/HeaderTabs";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
+import SearchBar from "../components/SearchBar";
+
+const YELP_API_KEY =
+  "mNGSMRW1wBIaWuyOr-q1_cBtMuPYbOU24hdsVEO8oLQ8aTQx67okpmhDbFEdztUnf4LyXEgi-1oYjiQ54IeH4qfDHU5mOtF7CEEmt5ASsLAPhcMczsU-T_hkFWR0YXYx";
 
 export default function Home() {
-    return (
-        <SafeAreaView style={{ backgroundColor:"#eee", flex:1 }}>
-            <View style={{ backgroundColor:"white", padding:15 }}>
-                <HeaderTabs />
-                <SearchBar />
-            </View>
-            <Categories />
-        </SafeAreaView>
-    )
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("Hollywood");
+  const [activeTab, setActiveTab] = useState("Delivery");
+
+  const getRestaurantsFromYelp = async () => {
+    const yelpURL = `https://api.yelp.com/v3/businesses/search?term=restaurant&location=${city}`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return await fetch(yelpURL, apiOptions)
+      .then((res) => res.json())
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [city, activeTab]);
+
+  return (
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <SearchBar cityHandler={setCity} />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItems restaurantData={restaurantData} />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+// Time stamp 1:39
