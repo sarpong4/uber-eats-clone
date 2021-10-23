9e04a88d897eeb4cc5288df51735d0b577e64b58
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-23 00:23:00 +0000

    Completed header tabs for Home Screen

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 
-export default function 
-() {
+export default function HeaderTabs() {
+    const [activeTab, setActiveTab] = useState("Delivery")
     return (
-        <View>
-            <Text></Text>
+        <View style={{ flexDirection:"row", alignSelf:"center" }}>
+            <HeaderButton 
+                text="Delivery"
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+            />
+            <HeaderButton 
+                text="Pickup"
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+            />
         </View>
     )
 }
+
+const HeaderButton = (props) => (
+    <TouchableOpacity style={{
+        backgroundColor: props.activeTab === props.text ? "black":"white",
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+        borderRadius: 30,
+        marginTop: 30, // This portion is optional, depends on phone used for test
+    }}
+    onPress = {() => props.setActiveTab(props.text)}>
+        <Text style={{
+            color: props.activeTab === props.text ? "white" : "black",
+            fontSize: 15,
+            fontWeight: "900",
+        }}>
+            {props.text}
+        </Text>
+    </TouchableOpacity>
+)
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+    return (
+        <SafeAreaView style={{ backgroundColor:"#eee", flex:1 }}>
+            <View style={{ backgroundColor:"white", padding:15 }}>
+                <HeaderTabs />
+            </View>
+        </SafeAreaView>
+    )
+}
