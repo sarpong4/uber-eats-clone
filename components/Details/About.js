c41eb1c4d38629ab51d65048f43e557715bf9ea2
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-24 06:22:47 +0000

    Added dynamic data reading from restaurantItems page, to load actual data, not from static array

--- components/Details/About.js
+++ components/Details/About.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { createFactory } from "react";
 import { View, Text, Image } from "react-native";
 
-const image =
-  "https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80";
+const yelpRestaurantInfo = {
+  name: "Farmhouse Kitchen Thai Cuisine",
+  image:
+    "https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+  price: "$$",
+  reviews: "1500",
+  rating: 4,
+  categories: [{ title: "Thai" }, { title: "Comfort Food" }, { title: "Soul Food"}],
+};
 
-const title = "Farmhouse Kitchen Thai Cuisine";
-const description = "Thai • Comfort Food • $$ •  🎟️ • 4 ⭐ (2913+)";
 
-export default function About() {
-  return (
-    <View>
+
+export default function About(props) {
+
+    const { name, image, price, reviews, rating, categories } = props.route.params;
+    
+    const formattedCategories = categories
+      .map((category) => category.title)
+      .join(" • ");
+    
+    const description = `${formattedCategories} ${
+      price ? " • " + price : ""
+    } • 🎫 • ${rating} ⭐ • (${reviews}+)`;
+
+    return (
+        <View>
       <RestaurantImage image={image} />
-      <RestaurantTitle title={title} />
+      <RestaurantName name={name} />
       <RestaurantDescription description={description} />
     </View>
   );
@@ -27,24 +44,28 @@ const RestaurantImage = (props) => (
   />
 );
 
-const RestaurantTitle = (props) => (
-    <Text style={{ 
-        fontSize: 27,
-        fontWeight: '600',
-        margintTop: 10,
-        marginHorizontal: 15,
-    }}>
-        {props.title}
-    </Text>
-)
+const RestaurantName = (props) => (
+  <Text
+    style={{
+      fontSize: 27,
+      fontWeight: "600",
+      marginTop: 10,
+      marginHorizontal: 15,
+    }}
+  >
+    {props.name}
+  </Text>
+);
 
 const RestaurantDescription = (props) => (
-    <Text style={{
-        marginTop: 10,
-        marginHorizontal: 15,
-        fontWeight: '400',
-        fontSize: 15
-    }}>
-        {props.description}
-    </Text>
-)
+  <Text
+    style={{
+      marginTop: 10,
+      marginHorizontal: 15,
+      fontWeight: "400",
+      fontSize: 15,
+    }}
+  >
+    {props.description}
+  </Text>
+);
