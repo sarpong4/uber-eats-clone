3c93488a18b7cd00599c01ce2371d56508d36dd5
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-24 06:26:07 +0000

    Made navigation changes

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './screens/Home';
-import RestaurantDetail from './screens/RestaurantDetail';
+import RootNavigation from './Navigation';
 
 export default function App() {
   return (
-    // <Home />
-    <RestaurantDetail />
+    <RootNavigation />
   );
 }
 
