a9a63b4a778d01c4a9871a2c6c1a8f42375f6fcc
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-26 15:42:27 +0000

    Wrapped navigation in redux

--- Navigation.js
+++ Navigation.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
-import Home from './screens/Home';
-import RestaurantDetail from './screens/RestaurantDetail';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
+import { Provider as ReduxProvider } from "react-redux";
+import configureStore from "./redux/store";
+
+const store = configureStore();
 
 export default function RootNavigation() {
+  const Stack = createStackNavigator();
 
-    const Stack = createStackNavigator();
+  const screenOptions = {
+    headerShown: false,
+  };
 
-    const screenOptions ={
-        headerShown: false,
-    }
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRoutesName="Home" screenOptions={screenOptions}>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRoutesName="Home" screenOptions={screenOptions}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
+  );
 }
