e9d777ce5f4afbe5ee1a474205eb69ae8eafe006
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-26 19:28:00 +0000

    Added transition from view cart button to checkout modal

--- components/Details/ViewCart.js
+++ components/Details/ViewCart.js
@@ -1,20 +1,114 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
+import OrderItem from "./OrderItem";
 
 export default function ViewCart() {
-  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const { items, restaurantName } = useSelector(
+    (state) => state.cartReducer.selectedItems
+  );
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
     .reduce((prev, curr) => prev + curr, 0);
 
-  const totalStr = `$${total.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}`
+  const formats = (amount) => {
+    return (
+      "$" +
+      String(
+        Number(amount)
+          .toFixed(2)
+          .replace(/\d(?=(\d{3})+\.)/g, "$&,")
+      )
+    );
+  };
 
+  const totalStr = formats(total);
 
-  console.log(totalStr);
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      justifyContent: "flex-end",
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+    },
+    modalCheckoutContainer: {
+      backgroundColor: "white",
+      padding: 16,
+      height: "65%",
+      borderWidth: 1,
+    },
+
+    restaurantName: {
+      textAlign: "center",
+      fontWeight: "600",
+      fontSize: 18,
+      marginBottom: 10,
+    },
+
+    subTotalContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 15,
+    },
+    subTotalText: {
+      textAlign: "left",
+      fontWeight: "600",
+      fontSize: 16,
+      marginBottom: 10,
+    },
+  });
+
+  const checkoutModalContent = () => {
+    return (
+      <>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalCheckoutContainer}>
+            <Text style={styles.restaurantName}>{restaurantName}</Text>
+            {items.map((item, index) => (
+              <OrderItem key={index} item={item} />
+            ))}
+            <View style={styles.subTotalContainer}>
+              <Text style={styles.subTotalText}>Subtotal</Text>
+              <Text>{totalStr}</Text>
+            </View>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <TouchableOpacity
+                style={{
+                  marginTop: 20,
+                  backgroundColor: "black",
+                  alignItems: "center",
+                  padding: 10,
+                  borderRadius: 30,
+                  width: "85%",
+                  position: "relative",
+                }}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={{ color: "white", fontSize: 20 }}>Checkout</Text>
+                <Text
+                  style={{ position: "absolute", right: 20, color: "white", fontSize: 15, top:15, }}
+                >
+                  {total ? totalStr : ""}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </>
+    );
+  };
 
   return (
     <>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        {checkoutModalContent()}
+      </Modal>
       {total ? (
         <View
           style={{
@@ -46,9 +140,14 @@ export default function ViewCart() {
                 justifyContent: "flex-end",
                 padding: 10,
               }}
+              onPress={() => setModalVisible(true)}
             >
-              <Text style={{ color: "white", fontSize: 20, marginRight: 30, }}>View Cart</Text>
-              <Text style={{ color: "white", fontSize: 20, marginRight: 10 }}>{totalStr}</Text>
+              <Text style={{ color: "white", fontSize: 20, marginRight: 30 }}>
+                View Cart
+              </Text>
+              <Text style={{ color: "white", fontSize: 20, marginRight: 10 }}>
+                {totalStr}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
